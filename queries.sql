40452abddaafb9d3d6aed636a0f2020ee4b71c7c
Hileamlak Mulugeta Yitayew <hilea.buisness@gmail.com>
2023-11-24 11:41:59 -0500

    here are the disease

--- queries.sql
+++ queries.sql
@@ -13,5 +13,42 @@ JOIN `mimmic_tables.D_ICD_DIAGNOSES` dd ON d.icd9_code = dd.icd9_code
 JOIN `mimmic_tables.PRESCRIPTIONS` p ON d.hadm_id = p.hadm_id
 WHERE dd.long_title LIKE '%sepsis%' OR dd.long_title LIKE '%septic shock%'
 
--- how do we want to group the diseases
--- and how do we find the price data for all these
+-- find disease code for ARDS
+SELECT icd9_code, long_title
+FROM `mimmic_tables.D_ICD_DIAGNOSES`
+WHERE long_title LIKE '%acute respiratory distress syndrome%'
+   OR long_title LIKE '%ARDS%'
+   OR long_title LIKE '%respiratory failure%'
+   OR long_title LIKE '%hypoxemia%'
+
+
+SELECT DISTINCT p.drug
+FROM `mimmic_tables.DIAGNOSES_ICD` d
+JOIN `mimmic_tables.D_ICD_DIAGNOSES` dd ON d.icd9_code = dd.icd9_code
+JOIN `mimmic_tables.PRESCRIPTIONS` p ON d.hadm_id = p.hadm_id
+WHERE dd.long_title LIKE '%acute respiratory distress syndrome%'
+   OR dd.long_title LIKE '%ARDS%'
+   OR dd.long_title LIKE '%respiratory failure%'
+   OR dd.long_title LIKE '%hypoxemia%'
+
+-- Cardiovascular Events
+SELECT icd9_code, long_title
+FROM `mimmic_tables.D_ICD_DIAGNOSES`
+WHERE long_title LIKE '%myocardial infarction%' OR long_title LIKE '%cardiac arrest%'
+
+SELECT DISTINCT p.drug
+FROM `mimmic_tables.DIAGNOSES_ICD` d
+JOIN `mimmic_tables.D_ICD_DIAGNOSES` dd ON d.icd9_code = dd.icd9_code
+JOIN `mimmic_tables.PRESCRIPTIONS` p ON d.hadm_id = p.hadm_id
+WHERE dd.long_title LIKE '%myocardial infarction%' OR dd.long_title LIKE '%cardiac arrest%'
+
+-- acute renal failure
+SELECT icd9_code, long_title
+FROM `mimmic_tables.D_ICD_DIAGNOSES`
+WHERE long_title LIKE '%acute renal failure%' OR long_title LIKE '%dialysis%'
+
+SELECT DISTINCT p.drug
+FROM `mimmic_tables.DIAGNOSES_ICD` d
+JOIN `mimmic_tables.D_ICD_DIAGNOSES` dd ON d.icd9_code = dd.icd9_code
+JOIN `mimmic_tables.PRESCRIPTIONS` p ON d.hadm_id = p.hadm_id
+WHERE dd.long_title LIKE '%acute renal failure%' OR dd.long_title LIKE '%dialysis%'
